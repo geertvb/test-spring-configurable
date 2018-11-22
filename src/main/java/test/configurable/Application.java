d503dd3e0afe9f972a6db4ffbb9382f09807b48b
geert <geert.van.bastelaere@gmail.com>
2018-11-22 23:54:15 +0100

    Use compile time weaving

--- src/main/java/test/configurable/Application.java
+++ src/main/java/test/configurable/Application.java
@@ -2,12 +2,10 @@ package test.configurable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @SpringBootApplication
 @EnableSpringConfigured
-@EnableLoadTimeWeaving
 public class Application {
 
     public static void main(String[] args) {
